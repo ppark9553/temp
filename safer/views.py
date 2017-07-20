257783885d6133ebf5e72d23e7d153b343ec98b3
Phillip <ppark9553@gmail.com>
2017-07-21 02:28:55 +0900

    changing schema

--- safer/views.py
+++ safer/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import render
 
-import simplejson as json
-
-def get_home_chart():
-    data_file = './static/safer/json/home.json'
-    with open(data_file) as f:
-        data = json.load(f)
-    return data
-
 # Create your views here.
 def deploy_test(request):
     return render(request, 'deploy_test.html', {})
 
 def index(request):
-    data = get_home_chart()
-    price = data['price']
-    volume = data['volume']
-    return render(request, 'safer/index.html', {'price': price, 'volume': volume})
+    return render(request, 'safer/index.html', {})
 
 def tinder(request):
     return render(request, 'safer/tinder_prac.html', {})
